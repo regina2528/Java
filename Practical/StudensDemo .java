7c4e3ed55c1360facd40dc18c689f7d3fdec4f45
regina2528 <113043330+regina2528@users.noreply.github.com>
2022-11-15 18:14:09 +0530

    Add files via upload

---
+++ Practical/StudensDemo .java
@@ -0,0 +1,75 @@
+// create student label in mysql using sno,marks,sname and insert 3 records using procedure
+
+
+sql query
+
+delimiter $$
+
+create procedure 'first1'.'studetails'
+(in sno int,in sname varchar(20),in marks int))
+
+begin
+
+insert into students values(sno,sname,marks);
+
+end $$
+
+
+
+jdbc program
+
+
+package first;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class StudensDemo {
+
+	public static void main(String[] args)throws SQLException,ClassNotFoundException, IOException
+	{
+		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/first1", "root", "admin");
+		Statement stmt=con.createStatement();
+		CallableStatement cs=(CallableStatement) con.prepareCall("{call studetails(?,?,?)}");
+		
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		while(true)
+		{
+			System.out.println("Enter sno");
+			String s1=br.readLine();
+			int sno=Integer.parseInt(s1);
+			System.out.println("Enter sname");
+			String sname=br.readLine();
+			System.out.println("Enter marks");
+			String s3=br.readLine();
+			int marks=Integer.parseInt(s3);
+			cs.setInt(1, sno);
+			cs.setString(2,sname);
+			cs.setInt(3,marks);
+			cs.executeUpdate();
+			System.out.println("u want to add more details y/n" );
+			String ans=br.readLine();
+			if(ans.equals("n"))
+				break;
+		}
+			
+			System.out.println("success");
+			con.close();
+		
+		
+				
+			
+		}
+		
+		
+
+	}
+
+
---
+++ Practical/Students12.java
@@ -0,0 +1,55 @@
+//prepare jdbc program to get student details along with column name
+
+sql query
+
+create table students(sno int,sname varchar(20),marks int)
+
+insert into students values(1,'regi',350),(2,'saki',400)
+
+select * from students
+
+
+
+jdbc program
+
+
+package first;
+
+
+import java.io.IOException;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+
+
+public class Students12 {
+
+	public static void main(String[] args) throws SQLException,ClassNotFoundException, IOException
+	{
+		
+		
+			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/first1", "root", "admin");
+			PreparedStatement stmt=con.prepareStatement("select * from students");
+			
+			ResultSet rs= stmt.executeQuery();
+			while(rs.next())
+			{
+				System.out.println(rs.getInt(1)+ "  "+rs.getString(2)+"   "+rs.getInt(3));
+			}
+			ResultSetMetaData rsmd=rs.getMetaData();
+			System.out.println("the columnname is:"+rsmd.getColumnName(1));
+			System.out.println("the columnname is:"+rsmd.getColumnName(2));
+			System.out.println("the columnname is:"+rsmd.getColumnName(3));
+			
+			
+			con.close();
+			
+			
+
+	}
+
+}
