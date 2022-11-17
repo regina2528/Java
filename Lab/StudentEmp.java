5320af42917854a2a5a1d8b7d493110c26086ccf
regina2528 <113043330+regina2528@users.noreply.github.com>
2022-11-17 18:33:55 +0530

    Add files via upload

---
+++ Lab/StudentEmp.java
@@ -0,0 +1,57 @@
+
+4.create student.txt files and store into emp table
+
+sql query
+
+create database employee
+
+use employee
+
+create table emp2(filename varchar(20),fileinfo longtext)
+
+select * from emp2
+
+jdbc program
+
+package first;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class FileEx {
+
+	public static void main(String[] args) throws SQLException,ClassNotFoundException, FileNotFoundException
+	{
+		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/employee", "root", "admin");
+		PreparedStatement stmt=con.prepareStatement("insert into emp2 values(?,?)");
+		File f=new File("e:\\Students.txt");
+		FileReader fr=new FileReader(f);
+		stmt.setString(1, "Student");
+		stmt.setCharacterStream(2, fr,(int)f.length());
+		int i=stmt.executeUpdate();
+		System.out.println(i+"row inserted");
+		con.close();
+		
+		
+	}
+
+}
+
+
+
+
+this is my student.txt file
+
+student details
+
+
+1 regi 480 computer science
+2 saki 390 Biology
+3 xyz 348 science
+4 suthahar 490 maths
+
---
+++ Lab/Students3.java
@@ -0,0 +1,87 @@
+
+1.prepare students table with sid,sname, marks,course in my sql
+
+
+
+sql query
+
+
+use students
+
+create table student(sid int,sname varchar(30),marks int,course varchar(50))
+
+
+2.insert,update,delete,select operations on student table
+
+insert into  student values(1,'regi',480,'computer science'),(2,'saki',390,'Biology'),(5,'sutha',500,'business')
+
+select * from student
+
+update student set sid=3 where sid=5
+
+delete from student where sid=3
+
+
+
+3.preparedstatement,statement interface methods on students table
+
+statement interface:
+
+
+package first;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Student1 {
+
+	public static void main(String[] args)throws SQLException,ClassNotFoundException
+	{
+		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/students", "root", "admin");
+		Statement stmt=con.createStatement();
+		ResultSet rs=stmt.executeQuery("select * from student");
+		while(rs.next()) {
+			System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getInt(3)+" "+rs.getString(4));
+			
+		}
+				
+		con.close();		
+	}
+
+
+
+preparedstatement:
+
+
+package first;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Student2 {
+
+	public static void main(String[] args)throws SQLException,ClassNotFoundException
+		{
+			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/students", "root", "admin");
+			PreparedStatement stmt=con.prepareStatement("insert into student values(?,?,?,?)");
+			stmt.setInt(1,4);
+			stmt.setString(2,"suthahar");
+			stmt.setInt(3, 490);
+			stmt.setString(4, "maths");
+			int i=stmt.executeUpdate();
+			System.out.println(i+"row inserted");
+			con.close();
+	}
+
+}
+
+
+
+
+}
